7da622a6c1758f80285d42d056e57299d559a1f1
Asutosh Palai <asupalai@gmail.com>
2014-10-29 23:32:07 +0530

    Solution for a glich in launching of GUI

--- main/WebLogin.java
+++ main/WebLogin.java
@@ -322,30 +322,33 @@ public class WebLogin extends javax.swing.JFrame {
 	}
 
 	public static void main(String args[]) {
-		
-		try {
-			for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-				if ("Nimbus".equals(info.getName())) {
-					javax.swing.UIManager.setLookAndFeel(info.getClassName());
-					break;
-				}
-			}
-		} catch (ClassNotFoundException ex) {
-			java.util.logging.Logger.getLogger(WebLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-		} catch (InstantiationException ex) {
-			java.util.logging.Logger.getLogger(WebLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-		} catch (IllegalAccessException ex) {
-			java.util.logging.Logger.getLogger(WebLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-		} catch (javax.swing.UnsupportedLookAndFeelException ex) {
-			java.util.logging.Logger.getLogger(WebLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-		
-		java.awt.EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				new WebLogin().setVisible(true);
-			}
-		});
-	}
-	}
+
+		   try {
+	            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+	                if ("Nimbus".equals(info.getName())) {
+	                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+	                    break;
+	                }
+	            }
+	        } catch (ClassNotFoundException ex) {
+	            java.util.logging.Logger.getLogger(WebLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+	        } catch (InstantiationException ex) {
+	            java.util.logging.Logger.getLogger(WebLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+	        } catch (IllegalAccessException ex) {
+	            java.util.logging.Logger.getLogger(WebLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+	        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+	            java.util.logging.Logger.getLogger(WebLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+	        }
+	        //</editor-fold>
+
+	        /* Create and display the form */
+	        java.awt.EventQueue.invokeLater(new Runnable() {
+	            public void run() {
+	                new WebLogin().setVisible(true);
+	            }
+	        });
+		}
+	
 
 	// Variables declaration - do not modify                     
 	private javax.swing.JButton jButton1;
